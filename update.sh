fa20830cec3f6cb87b6486477d96f1576b867006
Hexin <hexium310@gmail.com>
2020-02-03 18:10:19 +0900

    Fix sed

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 urls=$(curl -s https://raw.githubusercontent.com/Taraflex/Back2stackoverflow/master/back2stackoverflow.user.js | awk '/^\/\/ @match/ { print $3 }')
 for url in $urls; do
     grep "$url" google-search-censor.user.js > /dev/null ||
-    sed -i -e "/\/\/ insert before/i \ \ '$urls'," google-search-censor.user.js
+    sed -i -e "/\/\/ insert before/i \ \ '$url'," google-search-censor.user.js
 done
