fec67d2c005ff086d0464f8f12b189c1b2188945
Hexin <hexium310@gmail.com>
2020-02-03 14:18:03 +0900

    Refactor

--- google-search-censor.user.js
+++ google-search-censor.user.js
@@ -11,25 +11,26 @@
 // ==/UserScript==
 
 const blacklist = [
-    'sejuku.net',
-    'wa3.i-3-i.info',
-    'tutorialmore.*/',
-];
+  'sejuku.net',
+  'wa3.i-3-i.info',
+  'tutorialmore.com',
+]; // insert before
 
 const elements = document.getElementsByClassName("g");
 
 for (const element of elements) {
-    const link = element.querySelector('a[ping]').href;
-    for (const blacklistLink of blacklist) {
-        const blacklistRegExp = new RegExp(blacklistLink.replace(/(\.|\/)/g, '\\$1').replace(/\*/g, '.*'))
-        if (link.match(blacklistRegExp) === null) {
-            continue;
-        }
+  const link = element.querySelector('a[ping]').href;
+  const hiddenContent = document.createElement('div');
+  hiddenContent.insertAdjacentHTML('beforeend', 'This content was hidden.<br>');
+  hiddenContent.insertAdjacentHTML('beforeend', `<cite>${ link }</cite>`);
 
-        const hiddenContent = document.createElement('div');
-        hiddenContent.insertAdjacentHTML('beforeend', 'This content was hidden.<br>');
-        hiddenContent.insertAdjacentHTML('beforeend', `<cite>${ link }</cite>`);
-        element.firstElementChild.appendChild(hiddenContent);
-        element.getElementsByClassName('rc')[0].remove();
+  for (const blacklistLink of blacklist) {
+    const blacklistRegExp = new RegExp(blacklistLink.replace(/(\.|\/)/g, '\\$1').replace(/\*/g, '.*'))
+    if (!blacklistRegExp.test(link)) {
+      continue;
     }
+
+    element.firstElementChild.appendChild(hiddenContent);
+    element.getElementsByClassName('rc')[0].remove();
+  }
 }
