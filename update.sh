92355e2403a03fce7c961a2e0cc6f7afdccde050
Hexin <hexium310@gmail.com>
2020-02-05 16:40:41 +0900

    Enable updating version

--- update.sh
+++ update.sh
@@ -1,5 +1,15 @@
 urls=$(curl -s https://raw.githubusercontent.com/Taraflex/Back2stackoverflow/master/back2stackoverflow.user.js | awk '/^\/\/ @match/ { print $3 }')
+version=$(awk '/^\/\/ @version/ { print $3 }' google-search-censor.user.js)
+is_updated=0
 for url in $urls; do
-    grep -F "$url" google-search-censor.user.js > /dev/null ||
-    sed -i -e "/\/\/ insert before/i \ \ '$url'," google-search-censor.user.js
+    if grep -F "$url" google-search-censor.user.js > /dev/null; then
+      continue
+    fi
+
+    is_updated=1
+    gsed -i -e "/\/\/ insert before/i \ \ '$url'," google-search-censor.user.js
 done
+
+if (( is_updated )); then
+  gsed -i -e "s/\(\/\/ @version \+\).\+/\1${version%.*}.$(( ${version##*.} + 1 ))/" google-search-censor.user.js
+fi
