ea47b563fe24f0579bf8fc1e514c3ead3656a294
Hexin <hexium310@gmail.com>
2020-02-15 08:20:57 +0000

    Update blacklist

--- google-search-censor.user.js
+++ google-search-censor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         google-seach-censor
 // @include      /^(http|https):\/\/www\.google\..+\/search.*/
-// @version      0.1.2
+// @version      0.1.3
 // @namespace    hexium310
 // @author       Hexin
 // @downloadURL  https://raw.githubusercontent.com/hexium310/google-search-censor/master/google-search-censor.user.js
@@ -127,6 +127,18 @@ const blacklist = [
   'https://answeright.com/*',
   'https://www.answeright.com/*',
   'https://riptutorial'
+  'https://ffff65535.com/*/q/*',
+  'https://www.it-swarm.xyz/*/*',
+  'https://www.it-swarm.asia/*/*',
+  'https://www.it-swarm.dev/*/*',
+  'http://uwenku.com/question/*',
+  'https://uwenku.com/question/*',
+  'http://*.uwenku.com/question/*',
+  'https://*.uwenku.com/question/*',
+  'https://brokencontrollers.com/faq/*',
+  'https://www.brokencontrollers.com/faq/*',
+  'https://itdaan.com/blog/*',
+  'https://www.itdaan.com/blog/*',
 ]; // insert before
 
 const elements = document.getElementsByClassName("g");
